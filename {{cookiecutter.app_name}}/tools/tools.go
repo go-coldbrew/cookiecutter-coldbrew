46dda9adfcb22a4a9446c7128642c4da7ca81e94
Ankur Shrivastava <ankur@ankurs.com>
2025-02-21 16:57:42 +0800

    updating go versions and buf (#15)

--- {{cookiecutter.app_name}}/tools/tools.go
+++ {{cookiecutter.app_name}}/tools/tools.go
@@ -9,11 +9,6 @@ import (
 	_ "github.com/boumenot/gocover-cobertura"
 	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
-	_ "github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto"
 	_ "github.com/rakyll/statik"
 	_ "github.com/vektra/mockery/v2"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
