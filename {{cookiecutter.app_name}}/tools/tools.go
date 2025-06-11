5d8de90299d3b4ca2f0f7cfac7dca9f0f60a720c
Fajran Iman Rusadi <fajran.rusadi@gojek.com>
2025-06-11 08:59:04 +0700

    Replace statik with embed

--- {{cookiecutter.app_name}}/tools/tools.go
+++ {{cookiecutter.app_name}}/tools/tools.go
@@ -9,6 +9,5 @@ import (
 	_ "github.com/boumenot/gocover-cobertura"
 	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/rakyll/statik"
 	_ "github.com/vektra/mockery/v2"
 )
